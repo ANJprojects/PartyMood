7c8d7ce038eb87efafeacc90674e40479d319411
jyoti1493 <chouhanjyoti1412@gmail.com>
2017-09-23 19:04:38 +0530

    jecy

---
+++ imports/ui/components/login/login.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import template from './login.html';
+
+const name = 'login';
+export default angular.module(name, [
+        angularMeteor,
+        'accounts.ui'
+    ])
+    .component(name, {
+        template,
+        controllerAs: name
+    })  .config(config);
+
+  function config($stateProvider) {
+      'ngInject';
+      $stateProvider.state('login', {
+          url: '/login',
+          template: <login></login>
+      });
+  }
