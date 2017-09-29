1d55a0887c8752ec5f29d8ed3b9af96dfe2c0587
Ayush Chouksey <ayush.chouksey@yourmarch.com>
2017-09-29 23:47:09 +0530

    materialize selector jquery  patch added

--- imports/ui/components/partiesSort/partiesSort.js
+++ imports/ui/components/partiesSort/partiesSort.js
@@ -4,33 +4,37 @@ import angularMeteor from 'angular-meteor';
 import template from './partiesSort.html';
 
 class PartiesSort {
-  constructor($timeout) {
-    'ngInject';
 
-    $timeout(() => this.changed());
-  }
+    constructor($timeout, $scope, $reactive) {
+        'ngInject';
 
-  changed() {
-    this.onChange({
-      sort: {
-        [this.property]: parseInt(this.order)
-      }
-    });
-  }
+        $timeout(() => this.changed());
+        $timeout(function() {
+            $('select').material_select();
+        });
+    }
+
+    changed() {
+        this.onChange({
+            sort: {
+                [this.property]: parseInt(this.order)
+            }
+        });
+    }
 }
 
 const name = 'partiesSort';
 
 // create a module
 export default angular.module(name, [
-  angularMeteor
+    angularMeteor
 ]).component(name, {
-  template,
-  bindings: {
-    onChange: '&',
-    property: '@',
-    order: '@'
-  },
-  controllerAs: name,
-  controller: PartiesSort
+    template,
+    bindings: {
+        onChange: '&',
+        property: '@',
+        order: '@'
+    },
+    controllerAs: name,
+    controller: PartiesSort
 });
